0255f17a2211738b38b65d6bbbc39285b052c9a4
lal29 <lysaluceroa@gmail.com>
2024-04-07 15:51:48 -0700

    Login and Signup fixed layout

--- Project/signup.js
+++ Project/signup.js
@@ -60,43 +60,38 @@ const Main = () => {
       <input type="hidden" id="anPageName" name="page" value="signup" />
       <div class="container-center-horizontal">
         <div class="signup screen">
-          <div class="content">
-            <img
-              class="greentrans-1"
-              src="./public/greentrans-1.png"
-              alt="greentrans 1"
-            />
-            <div class="full-name_-input">
-              <div class="full-name poppins-medium-abbey-14px">Full Name</div>
-              <div class="overlap-group">
-                <div class="name poppins-normal-gray-13px">John Doe</div>
+          <form action="/register" method="POST">
+            <div class="content">
+              <img
+                class="greentrans-1"
+                src="./public/greentrans-1.png"
+                alt="greentrans 1"
+              />
+              <div class="full-name_-input">
+                <div class="full-name poppins-medium-abbey-14px">Full Name</div>
+                <input type="text" className="form-control" placeholder="Enter your name" name="fullname" />
               </div>
-            </div>
-            <div class="_-input">
-              <div class="email poppins-medium-abbey-14px">Email</div>
-              <div class="overlap-group">
-                <div class="enter-your-email-here poppins-normal-gray-13px">
-                  Enter your Email here
-                </div>
+              <div class="_-input">
+                <div class="email poppins-medium-abbey-14px">Email</div>
+                <input type="text" className="form-control" placeholder="Enter your email" name="email" />
               </div>
-            </div>
-            <div class="_-input">
-              <div class="password poppins-medium-abbey-14px">Password</div>
-              <div class="overlap-group">
-                <div class="enter-your-password poppins-normal-gray-13px">
-                  Enter your Password
-                </div>
+              <div class="_-input">
+                <div class="password poppins-medium-abbey-14px">Password</div>
+                <input type="password" className="form-control" placeholder="Enter your password" name="password" />
               </div>
+              <div class="_-input">
+                <div class="password poppins-medium-abbey-14px">Confirm Password</div>
+                <input type="password" className="form-control" placeholder="Confirm your password" name="confirmpassword" />
+              </div>
+              <button type="submit" class="btn_-create-account">SignUp</button>
+              <p class="have-an-account">
+                <span class="span0">Already have an account?</span>
+                <span class="span1">&nbsp;</span>
+                <a href="login.html" class="span2">Log in</a>
+              </p>
             </div>
-            <div class="btn_-create-account">
-              <div class="sign-up">SignUp</div>
-            </div>
-            <p class="have-an-account">
-              <span class="span0">Already have an account?</span>
-              <span class="span1">&nbsp;</span>
-              <span class="span2">Log in</span>
-            </p>
-          </div>
+          </form>
+
           <img
             class="chris-lee-70l1t-dai6r-m-unsplash-1"
             src="./public/chris-lee-70l1tdai6rm-unsplash-1.png"
@@ -156,9 +151,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="container-fluid p-0 d-flex" id="reactDiv">
-        <Header/>
+        <Header />
         <Main />
-        <Footer/>
+        <Footer />
       </div>
     );
   }
